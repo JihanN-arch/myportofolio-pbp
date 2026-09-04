0a6f61410b93271f7597e5c8b6b4c08e70921182
JihanN-arch <jnpermata07@gmail.com>
2026-09-05 03:02:01 +0700

    feat: tambah My Project section untuk nunjukin 3 projek sebelumnya - nambah reuseable card layout -implement project list dengan Django tempalte loop -add cardnya, img, link dsj

--- myportofolio_pbp/views.py
+++ myportofolio_pbp/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
+
 def landing_page(request):
-    return render(request, "index.html")
+    projects = [
+        {
+            "title": "My Old Portfolio",
+            "img": "/static/img/oldPortf.png",
+            "link": "https://webpofilejn.netlify.app/"
+        },
+        {
+            "title": "Arung's Website",
+            "img": "/static/img/arung.png",
+            "link": "https://arung.csui.dev/"
+        },
+        {
+            "title": "Skinzym",
+            "img": "/static/img/skinzym.png",
+            "link": "https://pkm-2026-fe.vercel.app/"
+        },
+    ]
+
+    return render(request, "index.html", {
+        "projects": projects
+    })
