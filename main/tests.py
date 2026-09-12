6f994810644fb8fc1af30677b7e7dc737a63de49
JihanN-arch <jnpermata07@gmail.com>
2026-09-13 01:18:05 +0700

    feat: nambahin reuseable empty state component

--- main/tests.py
+++ main/tests.py
@@ -47,7 +47,7 @@ class MainTest(TestCase):
         Experience.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
 
-        self.assertContains(response, "Belum ada pengalaman yang ditambahkan.")
+        self.assertContains(response, "Belum ada experience yang ditambahkan")
 
     def test_completed_experience(self):
         self.experience.ended_at = timezone.now()
@@ -55,7 +55,7 @@ class MainTest(TestCase):
         response = self.client.get(reverse("main:show_experience"))
 
         self.assertFalse(self.experience.is_ongoing)
-        self.assertContains(response, "Selesai")
+        self.assertContains(response, "-")
         self.assertNotContains(response, "Sedang berlangsung")
 
 class ShowMainViewTest(TestCase):
