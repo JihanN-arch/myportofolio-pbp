136e400570fb965b3eec1e291190c5b6ad6c7859
JihanN-arch <jnpermata07@gmail.com>
2026-09-13 10:10:06 +0700

    chore: update project settings and gitignore

--- myportofolio_pbp/settings.py
+++ myportofolio_pbp/settings.py
@@ -14,20 +14,19 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env.prod')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8%^mnmlb-ro8(%s8b++bcg0s%3#$%f!k%p=nk6#5#7=(7*vwjn'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jihan-nabiilah-myportofoliopbp.pws.cs.ui.ac.id"]
 
